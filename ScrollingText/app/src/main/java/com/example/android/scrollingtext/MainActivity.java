27616dad7141b5f9eb432fb0e294f92659657eb4
Alex Kasa <vailveix@gmail.com>
2018-11-21 17:00:19 -0500

    Android Cert Course 4:3 Dialogs and Pickers
    Tons of practice with input stuff.

--- ScrollingText/app/src/main/java/com/example/android/scrollingtext/MainActivity.java
+++ ScrollingText/app/src/main/java/com/example/android/scrollingtext/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.android.scrollingtext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView article_text = (TextView) findViewById(R.id.article);
+        registerForContextMenu(article_text);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.action_edit:
+                displayToast(getString(R.string.edit_select));
+                return true;
+            case R.id.action_delete:
+                displayToast(getString(R.string.delete_select));
+                return true;
+            case R.id.action_share:
+                displayToast(getString(R.string.share_select));
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
